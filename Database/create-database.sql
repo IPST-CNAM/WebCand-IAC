e2b4d186c582e97c8ae15f03e9cd145553da879b
Louis Richards <llouis.richards@gmail.com>
2023-07-06 22:11:00 +0200

    Added Database dockerfile + setup sql files

--- Database/create-database.sql
+++ Database/create-database.sql
@@ -4,8 +4,26 @@ CREATE DATABASE IF NOT EXISTS WebCand;
 -- Switch to the newly created database
 USE WebCand;
 
-CREATE TABLE Company(
-   id_company INT,
+CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'user';
+GRANT ALL PRIVILEGES ON WebCand.* TO 'user'@'%';
+FLUSH PRIVILEGES;
+
+-- The main User table
+CREATE TABLE IF NOT EXISTS RegisteredUser(
+   id_registered_user INT AUTO_INCREMENT,
+   email VARCHAR(256) NOT NULL,
+   first_name VARCHAR(200) NOT NULL,
+   last_name VARCHAR(200) NOT NULL,
+   birth_date INT NOT NULL,
+   password VARCHAR(256) NOT NULL,
+   phone_number VARCHAR(256) NOT NULL,
+   PRIMARY KEY(id_registered_user),
+   UNIQUE(email),
+   UNIQUE(phone_number)
+);
+
+CREATE TABLE IF NOT EXISTS Company(
+   id_company INT AUTO_INCREMENT,
    name VARCHAR(256) NOT NULL,
    business_sector VARCHAR(256) NOT NULL,
    size VARCHAR(256) NOT NULL,
@@ -13,60 +31,8 @@ CREATE TABLE Company(
    UNIQUE(name)
 );
 
-CREATE TABLE AdministrativeLevel(
-   Id_EnumAdministrativeLevel COUNTER,
-   role VARCHAR(256),
-   PRIMARY KEY(Id_EnumAdministrativeLevel)
-);
-
-CREATE TABLE SchoolSubject(
-   id_school_subject INT,
-   title VARCHAR(256) NOT NULL,
-   description TEXT,
-   credit INT NOT NULL,
-   PRIMARY KEY(id_school_subject)
-);
-
-CREATE TABLE TrainingCourse(
-   id_training_course INT,
-   title VARCHAR(256) NOT NULL,
-   description TEXT,
-   duration INT NOT NULL,
-   PRIMARY KEY(id_training_course)
-);
-
-CREATE TABLE RegisteredUser(
-   id_registered_user INT,
-   email VARCHAR(256) NOT NULL,
-   first_name VARCHAR(200) NOT NULL,
-   last_name VARCHAR(200) NOT NULL,
-   birth_date INT NOT NULL,
-   password VARCHAR(256) NOT NULL,
-   phone_number VARCHAR(256) NOT NULL,
-   Id_EnumAdministrativeLevel INT NOT NULL,
-   PRIMARY KEY(id_registered_user),
-   UNIQUE(email),
-   UNIQUE(phone_number),
-   FOREIGN KEY(Id_EnumAdministrativeLevel) REFERENCES AdministrativeLevel(Id_EnumAdministrativeLevel)
-);
-
-CREATE TABLE CompanyRepresentative(
-   id_registered_user INT,
-   id_company INT NOT NULL,
-   PRIMARY KEY(id_registered_user),
-   FOREIGN KEY(id_registered_user) REFERENCES RegisteredUser(id_registered_user),
-   FOREIGN KEY(id_company) REFERENCES Company(id_company)
-);
-
-CREATE TABLE ApplicationStatus(
-   Id_ApplicationStatus COUNTER,
-   status VARCHAR(256) NOT NULL,
-   PRIMARY KEY(Id_ApplicationStatus),
-   UNIQUE(status)
-);
-
-CREATE TABLE CompanyOffer(
-   id_company_offer INT,
+CREATE TABLE IF NOT EXISTS CompanyOffer(
+   id_company_offer INT AUTO_INCREMENT,
    duration INT NOT NULL,
    start_date INT NOT NULL,
    required_degree_level VARCHAR(256) NOT NULL,
@@ -74,16 +40,65 @@ CREATE TABLE CompanyOffer(
    description TEXT,
    id_registered_user INT NOT NULL,
    PRIMARY KEY(id_company_offer),
-   FOREIGN KEY(id_registered_user) REFERENCES CompanyRepresentative(id_registered_user)
+   FOREIGN KEY(id_registered_user) REFERENCES RegisteredUser(id_registered_user)
 );
 
-CREATE TABLE Teacher(
+CREATE TABLE IF NOT EXISTS SchoolSubject(
+   id_school_subject INT AUTO_INCREMENT,
+   title VARCHAR(256) NOT NULL,
+   description TEXT,
+   credit INT NOT NULL,
+   PRIMARY KEY(id_school_subject)
+);
+
+CREATE TABLE IF NOT EXISTS TrainingCourse(
+   id_training_course INT AUTO_INCREMENT,
+   title VARCHAR(256) NOT NULL,
+   description TEXT,
+   duration INT NOT NULL,
+   PRIMARY KEY(id_training_course)
+);
+
+CREATE TABLE IF NOT EXISTS Application(
+   id_application INT AUTO_INCREMENT,
+   application_date INT NOT NULL,
+   status VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_application)
+);
+
+CREATE TABLE IF NOT EXISTS Video(
+   id_video INT AUTO_INCREMENT,
+   url VARCHAR(256) NOT NULL,
+   id_application INT NOT NULL,
+   PRIMARY KEY(id_video),
+   FOREIGN KEY(id_application) REFERENCES Application(id_application)
+);
+
+CREATE TABLE IF NOT EXISTS CoverLetter(
+   id_cover_letter INT AUTO_INCREMENT,
+   url VARCHAR(256) NOT NULL,
+   id_application INT NOT NULL,
+   PRIMARY KEY(id_cover_letter),
+   FOREIGN KEY(id_application) REFERENCES Application(id_application)
+);
+
+-- Note: The id_registered_user values are the id of the RegisteredUser
+-- the id_company values are the id of the Company that the RegisteredUser works for
+CREATE TABLE IF NOT EXISTS CompanyRepresentative(
+   id_registered_user INT,
+   id_company INT NOT NULL,
+   PRIMARY KEY(id_registered_user),
+   FOREIGN KEY(id_registered_user) REFERENCES RegisteredUser(id_registered_user),
+   FOREIGN KEY(id_company) REFERENCES Company(id_company)
+);
+
+CREATE TABLE IF NOT EXISTS Teacher(
    id_registered_user INT,
    PRIMARY KEY(id_registered_user),
    FOREIGN KEY(id_registered_user) REFERENCES RegisteredUser(id_registered_user)
 );
 
-CREATE TABLE Candidate(
+CREATE TABLE IF NOT EXISTS Candidate(
    id_registered_user INT,
    ine_number INT NOT NULL,
    last_degree VARCHAR(160),
@@ -95,41 +110,46 @@ CREATE TABLE Candidate(
    FOREIGN KEY(id_registered_user) REFERENCES RegisteredUser(id_registered_user)
 );
 
-CREATE TABLE AdmittedCandidate(
+CREATE TABLE IF NOT EXISTS AdmittedCandidate(
    id_registered_user INT,
    internal_email VARCHAR(256) NOT NULL,
    PRIMARY KEY(id_registered_user),
    FOREIGN KEY(id_registered_user) REFERENCES Candidate(id_registered_user)
 );
 
-CREATE TABLE EducationManager(
+CREATE TABLE IF NOT EXISTS Administrative(
+   id_registered_user INT,
+   role VARCHAR(256),
+   PRIMARY KEY(id_registered_user),
+   FOREIGN KEY(id_registered_user) REFERENCES RegisteredUser(id_registered_user)
+);
+
+CREATE TABLE IF NOT EXISTS EducationManager(
    id_registered_user INT,
    PRIMARY KEY(id_registered_user),
    FOREIGN KEY(id_registered_user) REFERENCES RegisteredUser(id_registered_user)
 );
 
-CREATE TABLE RecruitmentSession(
-   id_recruitement_session INT,
-   name VARCHAR(256) NOT NULL,
+CREATE TABLE IF NOT EXISTS RecruitmentSession(
+   id_recruitement_session INT AUTO_INCREMENT,
    start_date INT NOT NULL,
    end_date INT NOT NULL,
-   _open LOGICAL,
    id_registered_user INT NOT NULL,
    PRIMARY KEY(id_recruitement_session),
    FOREIGN KEY(id_registered_user) REFERENCES EducationManager(id_registered_user)
 );
 
-CREATE TABLE SchoolGrade(
-   id_school_note INT,
+CREATE TABLE IF NOT EXISTS SchoolNote(
+   id_school_note INT AUTO_INCREMENT,
    school_subject VARCHAR(256) NOT NULL,
-   grade DOUBLE NOT NULL,
+   note DECIMAL(15,2) NOT NULL,
    id_registered_user INT NOT NULL,
    PRIMARY KEY(id_school_note),
    FOREIGN KEY(id_registered_user) REFERENCES Candidate(id_registered_user)
 );
 
-CREATE TABLE JobExperience(
-   id_job_experience INT,
+CREATE TABLE IF NOT EXISTS JobExperience(
+   id_job_experience INT AUTO_INCREMENT,
    company_name VARCHAR(256),
    job_position_name VARCHAR(256) NOT NULL,
    job_description TEXT NOT NULL,
@@ -140,52 +160,15 @@ CREATE TABLE JobExperience(
    FOREIGN KEY(id_registered_user) REFERENCES Candidate(id_registered_user)
 );
 
-CREATE TABLE Resume(
-   id_resume INT,
+CREATE TABLE IF NOT EXISTS Resume(
+   id_resume INT AUTO_INCREMENT,
    url VARCHAR(256) NOT NULL,
    id_registered_user INT NOT NULL,
    PRIMARY KEY(id_resume),
    FOREIGN KEY(id_registered_user) REFERENCES Candidate(id_registered_user)
 );
 
-CREATE TABLE Application(
-   id_application INT,
-   application_date DATE NOT NULL,
-   Id_ApplicationStatus INT NOT NULL,
-   id_recruitement_session INT NOT NULL,
-   id_registered_user INT NOT NULL,
-   PRIMARY KEY(id_application),
-   FOREIGN KEY(Id_ApplicationStatus) REFERENCES ApplicationStatus(Id_ApplicationStatus),
-   FOREIGN KEY(id_recruitement_session) REFERENCES RecruitmentSession(id_recruitement_session),
-   FOREIGN KEY(id_registered_user) REFERENCES Candidate(id_registered_user)
-);
-
-CREATE TABLE Video(
-   id_video INT,
-   url VARCHAR(256) NOT NULL,
-   id_application INT NOT NULL,
-   PRIMARY KEY(id_video),
-   FOREIGN KEY(id_application) REFERENCES Application(id_application)
-);
-
-CREATE TABLE CoverLetter(
-   id_cover_letter INT,
-   url VARCHAR(256) NOT NULL,
-   id_application INT NOT NULL,
-   PRIMARY KEY(id_cover_letter),
-   FOREIGN KEY(id_application) REFERENCES Application(id_application)
-);
-
-CREATE TABLE Rating(
-   Id_Notation COUNTER,
-   grade DOUBLE,
-   description VARCHAR(256),
-   id_application INT NOT NULL,
-   PRIMARY KEY(Id_Notation),
-   FOREIGN KEY(id_application) REFERENCES Application(id_application)
-);
-
-CREATE TABLE TeacherInstructs(
+CREATE TABLE IF NOT EXISTS TeacherInstructs(
    id_registered_user INT,
    id_training_course INT,
    PRIMARY KEY(id_registered_user, id_training_course),
@@ -193,10 +176,18 @@ CREATE TABLE TeacherInstructs(
    FOREIGN KEY(id_training_course) REFERENCES TrainingCourse(id_training_course)
 );
 
-CREATE TABLE CourseSubject(
+CREATE TABLE IF NOT EXISTS CourseSubject(
    id_school_subject INT,
    id_training_course INT,
    PRIMARY KEY(id_school_subject, id_training_course),
    FOREIGN KEY(id_school_subject) REFERENCES SchoolSubject(id_school_subject),
    FOREIGN KEY(id_training_course) REFERENCES TrainingCourse(id_training_course)
+);
+
+CREATE TABLE IF NOT EXISTS Apply(
+   id_registered_user INT,
+   id_application INT,
+   PRIMARY KEY(id_registered_user, id_application),
+   FOREIGN KEY(id_registered_user) REFERENCES Candidate(id_registered_user),
+   FOREIGN KEY(id_application) REFERENCES Application(id_application)
 );
---
+++ Database/populate-database.sql
@@ -0,0 +1,150 @@
+-- Insert sample data into SchoolSubject table
+INSERT INTO SchoolSubject (title, description, credit)
+VALUES
+    ('Mathematics', 'Advanced mathematics course', 4),
+    ('Computer Science', 'Introduction to computer programming', 3),
+    ('Physics', 'Basic principles of physics', 3);
+
+-- Insert sample data into TrainingCourse table
+INSERT INTO TrainingCourse (title, description, duration)
+VALUES
+    ('Web Development', 'Learn web development technologies', 12),
+    ('Data Science', 'Introduction to data analysis and machine learning', 8),
+    ('Digital Marketing', 'Master digital marketing strategies', 6);
+
+-- Insert sample data into RegisteredUser table
+INSERT INTO RegisteredUser (email, first_name, last_name, birth_date, password, phone_number)
+VALUES
+    ('john@example.com', 'John', 'Doe', UNIX_TIMESTAMP('1990-05-15'), 'password123', '1234567890'),
+    ('jane@example.com', 'Jane', 'Smith', UNIX_TIMESTAMP('1992-09-20'), 'pass456', '9876543210'),
+    ('alice@example.com', 'Alice', 'Johnson', UNIX_TIMESTAMP('1988-12-31'), 'pass789', '4567891230'),
+    ('bob@example.com', 'Bob', 'Williams', UNIX_TIMESTAMP('1994-06-25'), 'pass012', '3216549870'),
+    ('emma@example.com', 'Emma', 'Brown', UNIX_TIMESTAMP('1997-03-18'), 'pass345', '9873216540'),
+    ('michael@example.com', 'Michael', 'Davis', UNIX_TIMESTAMP('1991-08-22'), 'pass678', '6549873210'),
+    ('sophia@example.com', 'Sophia', 'Miller', UNIX_TIMESTAMP('1995-11-12'), 'pass901', '3210987650'),
+    ('alex@example.com', 'Alex', 'Wilson', UNIX_TIMESTAMP('1993-02-07'), 'pass234', '6540129870'),
+    ('olivia@example.com', 'Olivia', 'Thompson', UNIX_TIMESTAMP('1998-09-02'), 'pass567', '9870123450'),
+    ('william@example.com', 'William', 'Jones', UNIX_TIMESTAMP('1996-04-09'), 'pass890', '6543210980');
+
+-- Insert sample data into Company table
+INSERT INTO Company (name, business_sector, size)
+VALUES
+    ('ABC Company', 'IT Services', 'Large'),
+    ('XYZ Corporation', 'Finance', 'Medium'),
+    ('Z Corporation', 'Game', 'Small');
+
+-- Insert sample data into CompanyOffer table
+INSERT INTO CompanyOffer (duration, start_date, required_degree_level, title, description, id_registered_user)
+VALUES
+    (6, UNIX_TIMESTAMP('2023-06-01'), 'Bachelor', 'Software Developer', 'Develop web applications', 1),
+    (12, UNIX_TIMESTAMP('2023-07-01'), 'Master', 'Data Analyst', 'Analyzing and visualizing data', 2);
+
+-- Insert sample data into CompanyRepresentative 
+-- Note: The id_registered_user values are the id of the RegisteredUser
+-- the id_company values are the id of the Company 
+INSERT INTO CompanyRepresentative (id_registered_user, id_company)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3);
+
+-- Insert sample data into Teacher table
+INSERT INTO Teacher (id_registered_user)
+VALUES
+    (3),
+    (4);
+
+-- Insert sample data into Candidate table
+INSERT INTO Candidate (id_registered_user, ine_number, last_degree, work_experiences, gender, skills)
+VALUES
+    (5, 123456, 'Bachelor of Science', 'Internship at ABC Company', 'M', 'Programming, Problem-solving'),
+    (6, 789012, 'Master of Business Administration', '2 years as a marketing assistant', 'F', 'Marketing, Analytics'),
+    (7, 345678, 'Bachelor of Engineering', 'Freelance web developer', 'M', 'Web Development, UI/UX Design'),
+    (8, 901234, 'Bachelor of Arts', 'Customer service representative', 'F', 'Communication, Problem-solving'),
+    (9, 567890, 'Master of Science', 'Research assistant at university', 'M', 'Data Analysis, Statistics'),
+    (10, 234567, 'Bachelor of Business Administration', 'Marketing intern at a startup', 'F', 'Digital Marketing, Social Media');
+
+-- Insert sample data into AdmittedCandidate table
+-- Note: The internal_email values are the email addresses of the admitted candidates
+-- the id_registered_user values are the id of the Candidate
+INSERT INTO AdmittedCandidate (id_registered_user, internal_email)
+VALUES
+    (5, 'john@example.com'),
+    (6, 'jane@example.com'),
+    (7, 'alice@example.com');
+
+-- Insert sample data into Application table
+INSERT INTO Application (application_date, status)
+VALUES
+    (UNIX_TIMESTAMP('2022-05-26'), 'Submitted'),
+    (UNIX_TIMESTAMP('2022-06-05'), 'In Review'),
+    (UNIX_TIMESTAMP('2022-06-15'), 'Rejected');
+
+-- Insert sample data into Apply table
+INSERT INTO Apply (id_registered_user, id_application)
+VALUES (5, 1),
+       (6, 2),
+       (7, 3);
+
+-- Insert sample data into Video table
+INSERT INTO Video (url, id_application)
+VALUES ('video1_url', 1),
+       ('video2_url', 2),
+       ('video3_url', 3);
+
+-- Insert sample data into CoverLetter table
+INSERT INTO CoverLetter (url, id_application)
+VALUES ('cover_letter1_url', 1),
+       ('cover_letter2_url', 2),
+       ('cover_letter3_url', 3);
+
+-- Insert sample data into Administrative table
+INSERT INTO Administrative (id_registered_user, role)
+VALUES
+    (1, 'Admin Assistant'),
+    (2, 'HR Manager');
+
+-- Insert sample data into EducationManager table
+INSERT INTO EducationManager (id_registered_user)
+VALUES
+    (3),
+    (4);
+
+-- Insert sample data into RecruitmentSession table
+-- Note: The start_date and end_date values are in Unix timestamp format
+-- the id_registered_user values are the id of the EducationManager
+INSERT INTO RecruitmentSession (start_date, end_date, id_registered_user)
+VALUES
+    (UNIX_TIMESTAMP('2023-08-01'), UNIX_TIMESTAMP('2023-08-31'), 3),
+    (UNIX_TIMESTAMP('2023-09-01'), UNIX_TIMESTAMP('2023-09-30'), 4);
+
+-- Insert sample data into SchoolNote table
+INSERT INTO SchoolNote (school_subject, note, id_registered_user)
+VALUES
+    ('Mathematics', 90.5, 5),
+    ('Analysis', 85.0, 5),
+    ('Development', 92.3, 6);
+
+-- Insert sample data into JobExperience table
+INSERT INTO JobExperience (company_name, job_position_name, job_description, start_date, end_date, id_registered_user)
+VALUES
+    ('ABC Company', 'Software Developer Intern', 'Developed web applications using HTML, CSS, and JavaScript', UNIX_TIMESTAMP('2021-07-01'), UNIX_TIMESTAMP('2022-01-31'), 5),
+    ('XYZ Corporation', 'Marketing Assistant', 'Assisted in creating and implementing digital marketing campaigns', UNIX_TIMESTAMP('2020-08-01'), UNIX_TIMESTAMP('2022-06-30'), 6);
+
+-- Insert sample data into Resume table
+INSERT INTO Resume (url, id_registered_user)
+VALUES
+    ('http://example.com/resume1.pdf', 5),
+    ('http://example.com/resume2.pdf', 6);
+
+-- Insert sample data into TeacherInstructs table
+INSERT INTO TeacherInstructs (id_registered_user, id_training_course)
+VALUES
+    (3, 1),
+    (4, 2);
+
+-- Insert sample data into CourseSubject table
+INSERT INTO CourseSubject (id_school_subject, id_training_course)
+VALUES
+    (1, 1),
+    (2, 1),
+    (3, 2);
